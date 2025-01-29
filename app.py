f27e0d243ce7a16f830d61930f8df01aa28819cf
FarhanAli97 <farhan.pro.pk@gmail.com>
2025-01-29 23:18:08 +0500

    change branch

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ def addition(a,b):
 print(addition(2,3))
 print(addition(4,5))
 
+def subtraction(a,b):
+    return(a-b)
+print(subtraction(2,3))
+
+def multiplication(a,b):
+    return(a*b)
+print(50,5)
+
