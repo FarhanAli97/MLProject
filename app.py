798561718ed012cb29866da268701544a20c615c
FarhanAli97 <farhan.pro.pk@gmail.com>
2025-01-31 22:13:22 +0500

    update app.py file

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from src.MLProject.exceptions import CustomException
 import sys
 
 
-
-
-
 if __name__=="__main__":
     logging.info("THE EXECUTION HAS STARTED")
 
 try:
     a=1/0
 except Exception as e:
+    logging.error(f"An error occurred: {e}")
     raise CustomException(e, sys)
